3f41e95c9582fdf3622777db127fabb13c8c1065
mefesto <tosha.g@mail.ru>
2025-09-11 10:41:14 +0300

    добавил страницу поиска станций

--- src/main/java/mobile/pages/searchTrains/SearchStationPage.java
+++ src/main/java/mobile/pages/searchTrains/SearchStationPage.java
@@ -1,4 +1,47 @@
 package mobile.pages.searchTrains;
 
+import com.codeborne.selenide.appium.SelenideAppiumElement;
+import io.appium.java_client.AppiumBy;
+import io.qameta.allure.Step;
+import mobile.pages.main.MainPage;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.appium.SelenideAppium.$;
+
 public class SearchStationPage {
+
+    private final SelenideAppiumElement
+            inputSearch = $(AppiumBy.id("SelenideAppiumElement")),
+            tabCitiesAndStation = $(AppiumBy.id("ru.rzd.pass.debug:id/first_type_view")),
+            tapRoutes = $(AppiumBy.id("ru.rzd.pass.debug:id/second_type_view"));
+
+    @Step("Проверка начальных элементов страницы")
+    public SearchStationPage checkInitElements(){
+        inputSearch.shouldBe(visible);
+        tapRoutes.shouldBe(visible);
+        tabCitiesAndStation.shouldBe(visible);
+        return this;
+    }
+
+    @Step("Заполнить поле поиска: {value}")
+    public SearchStationPage setStation(String value){
+        inputSearch.setValue(value);
+        return this;
+    }
+
+    public MainPage clickOnElement (String value){
+        if($(AppiumBy.xpath("//android.widget.TextView[contains(@resource-id,'id/tvTitle') and @text='" + value + "']")).is(visible)){
+            $(AppiumBy.xpath("//android.widget.TextView[contains(@resource-id,'id/tvTitle') and @text='" + value + "']")).click();
+        }
+        else{
+            $(AppiumBy.xpath("//android.widget.TextView[contains(@resource-id,'id/tvTitle') and @text='" + value + "']"))
+                    .scrollTo()
+                    .shouldBe(visible)
+                    .click();
+        }
+        inputSearch.shouldNotBe(visible);
+        return new MainPage();
+    }
 }
