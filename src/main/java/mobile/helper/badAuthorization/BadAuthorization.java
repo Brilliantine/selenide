b03162ddc55fbc913fa6a2179c0e0ae144ef7fa0
mefesto <tosha.g@mail.ru>
2026-04-07 16:33:25 +0300

    Добавил authorizationBad() для обхода бага с зависанием главного экрана

--- src/main/java/mobile/helper/badAuthorization/BadAuthorization.java
+++ src/main/java/mobile/helper/badAuthorization/BadAuthorization.java
@@ -1,7 +1,19 @@
 package mobile.helper.badAuthorization;
 
+import mobile.helper.LoginHelper;
 import mobile.pages.base.BasePage;
 
 //Класс нужен для обхода бага с подвисанием экрана авторизации
 public class BadAuthorization extends BasePage {
+
+    public void authorizationBad(){
+        try {
+            LoginHelper.authorization();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("authorization() упал по таймауту");
+            pressBack();
+
+        }
+    }
 }
