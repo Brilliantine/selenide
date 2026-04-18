427297c322581600ff7bf50c095fee080ed9b8da
Brilliantine <tosha.g@mail.ru>
2026-04-18 14:54:34 +0300

    В PayPage переписал пути в локаторах на использования androidUIAutomator, т.к id почему-то не работает на ПШ

--- src/main/java/mobile/pages/pay/PayPage.java
+++ src/main/java/mobile/pages/pay/PayPage.java
@@ -11,19 +11,19 @@ import static com.codeborne.selenide.appium.SelenideAppium.$;
 public class PayPage extends BasePage {
     private final SelenideAppiumElement
             title = $(AppiumBy.androidUIAutomator("new UiSelector().text(\"Оплата\")")),
-            btnPay = $(AppiumBy.id("OK")),
-            fieldNumberCard = $(AppiumBy.id("cp-pan-decor")),
-            fieldExpirationDate = $(AppiumBy.id("cc-exp-month")),
-            fieldCVV = $(AppiumBy.id("cvv2")),
+            btnPay = $(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"OK\")")),
+            fieldNumberCard = $(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"cp-pan-decor\")")),
+            fieldExpirationDate = $(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"cc-exp-month\")")),
+            fieldCVV = $(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"cvv2\")")),
             btnBack = $(AppiumBy.androidUIAutomator("new UiSelector().text(\"← Вернуться в магазин\")"));
 
     @Step("Проверка начальных элементов экрана 'Оплата'")
     public PayPage checkInitElements(){
         title.shouldBe(visible);
-        /*btnPay.shouldBe(visible);
+        btnPay.shouldBe(visible);
         fieldNumberCard.shouldBe(visible);
         fieldExpirationDate.shouldBe(visible);
-        fieldCVV.shouldBe(visible);*/
+        fieldCVV.shouldBe(visible);
         return this;
     }
 
